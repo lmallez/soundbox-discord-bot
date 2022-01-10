d42c8153f66587916dc55517a7ed7f1265caf7aa
Louis Mallez <louis.mallez@epitech.eu>
2022-01-10 17:03:55 +0100

    lint

--- main.py
+++ main.py
@@ -18,69 +18,70 @@ client = commands.Bot(command_prefix=BOT_COMMAND_PREFIX, description=BOT_DESCRIP
 
 @client.event
 async def on_ready():
-	print("Logged in as {} (#{})".format(client.user.name, client.user.id))
+    print("Logged in as {} (#{})".format(client.user.name, client.user.id))
 
 
 def get_sounds():
-	return os.listdir(BOT_SOUNDS_PATH)
+    return os.listdir(BOT_SOUNDS_PATH)
 
 
 @client.command(pass_context=True, no_pm=True)
 async def sound_dc(ctx):
-	vc = get(ctx.bot.voice_clients, guild=ctx.guild)
-	if vc:
-		await vc.disconnect()
+    vc = get(ctx.bot.voice_clients, guild=ctx.guild)
+    if vc:
+        await vc.disconnect()
 
 
 @client.command(pass_context=True, no_pm=True)
 async def sound_list(ctx):
-	sounds = [s.split(".")[0] for s in get_sounds()]
-	await ctx.message.reply("```{}```".format("\n".join(sounds)))
+    sounds = [s.split(".")[0] for s in get_sounds()]
+    await ctx.message.reply("```{}```".format("\n".join(sounds)))
 
 
 class NotFound(Exception):
-	def __init__(self, sound: str):
-		self.sound = sound
+    def __init__(self, sound: str):
+        self.sound = sound
 
 
 def sound_selected(sounds: list, args: list):
-	audios = ["{}.mp3".format(arg) for arg in args]
-	for audio in audios:
-		if audio not in sounds:
-			raise NotFound(audio)
-	return audios
+    audios = ["{}.mp3".format(arg) for arg in args]
+    for audio in audios:
+        if audio not in sounds:
+            raise NotFound(audio)
+    return audios
 
 
 def sound_random(sounds: list):
-	return [sounds[randint(0, len(sounds))]]
+    return [sounds[randint(0, len(sounds))]]
 
 
 @client.command(pass_context=True, no_pm=True)
 async def sound(ctx):
-	if not ctx.message.author.voice:
-		await ctx.message.reply(BOT_ERROR_NOT_CONNECTED)
-		return
-	channel = ctx.message.author.voice.channel
-	sounds = get_sounds()
-	args = ctx.args[0].message.clean_content.split(" ")[1:]
-	if len(args) > 0:
-		try:
-			audios = sound_selected(sounds, args)
-		except NotFound as e:
-			await ctx.message.reply(BOT_ERROR_NOT_FOUND.format(e.sound))
-			return
-	else:
-		audios = sound_random(sounds)
-	vc = get(ctx.bot.voice_clients, guild=ctx.guild)
-	if not vc:
-		vc = await channel.connect()
-	await ctx.message.add_reaction(
-		str(BOT_REACTIONS[randint(0, len(BOT_REACTIONS) - 1)])
-	)
-	for audio in audios:
-		vc.play(discord.FFmpegPCMAudio("sounds/{}".format(audio)))
-		while vc.is_playing():
-			sleep(0.1)
+    if not ctx.message.author.voice:
+        await ctx.message.reply(BOT_ERROR_NOT_CONNECTED)
+        return
+    channel = ctx.message.author.voice.channel
+    sounds = get_sounds()
+    args = ctx.args[0].message.clean_content.split(" ")[1:]
+    if len(args) > 0:
+        try:
+            audios = sound_selected(sounds, args)
+        except NotFound as e:
+            await ctx.message.reply(BOT_ERROR_NOT_FOUND.format(e.sound))
+            return
+    else:
+        audios = sound_random(sounds)
+    vc = get(ctx.bot.voice_clients, guild=ctx.guild)
+    if not vc:
+        vc = await channel.connect()
+    await ctx.message.add_reaction(
+        str(BOT_REACTIONS[randint(0, len(BOT_REACTIONS) - 1)])
+    )
+    for audio in audios:
+        vc.play(discord.FFmpegPCMAudio("sounds/{}".format(audio)))
+        while vc.is_playing():
+            sleep(0.1)
+
 
 token = os.environ.get("DISCORD_TOKEN")
 client.run(token)
